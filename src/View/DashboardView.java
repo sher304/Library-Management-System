c1813d372858696822ad1d0dd0555b2c65372739
sher304 <esherovshermat@gmail.com>
2025-01-13 20:28:20 +0100

    Added Dashboard View, with possibilities for Manager and User

--- src/View/DashboardView.java
+++ src/View/DashboardView.java
@@ -6,36 +6,62 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Objects;
 
 public class DashboardView extends JFrame {
 
     private DefaultTableModel booksTable = new DefaultTableModel();
 
     public DashboardView() {
+        setColumnNames();
+        setData();
+
         setTitle("Library Management System");
         setSize(800, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
 
+        JPanel upperPanel = new JPanel();
+        upperPanel.setBackground(Color.white);
+        upperPanel.add(new Label("Dashboard"));
+
         JPanel leftPanel = new JPanel();
         leftPanel.setLayout(new BoxLayout(leftPanel, BoxLayout.Y_AXIS));
-        leftPanel.add(createManagerPanel());
+//        leftPanel.add(createManagerPanel());
+        leftPanel.add(createUserLeftPanel());
 
+        // Right Table
+        JPanel rightPanel = new JPanel();
+        JLabel informLabel = new JLabel("Information about books");
+        rightPanel.add(informLabel, BorderLayout.NORTH);
+
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.Y_AXIS));
+        JTextField searchField = new JTextField("");
+        searchPanel.add(searchField);
+        searchPanel.add(new Button("Search book"));
+
+        JTable table = new JTable(booksTable);
+        JScrollPane tableScrollPane = new JScrollPane(table);
+        rightPanel.add(tableScrollPane, BorderLayout.CENTER);
+        rightPanel.add(searchPanel, BorderLayout.SOUTH);
+
+        add(upperPanel, BorderLayout.NORTH);
         add(leftPanel, BorderLayout.WEST);
+        add(rightPanel);
     }
 
     private static JPanel createManagerPanel() {
         JPanel parentPanel = new JPanel();
-        parentPanel.setLayout(new GridLayout(6, 1));
-        parentPanel.setBackground(Color.red);
+        parentPanel.setLayout(new GridLayout(7, 1));
+        parentPanel.setBorder(BorderFactory.createLineBorder(Color.black));
 
         JPanel bookTitlePanel = new JPanel();
         bookTitlePanel.setLayout(new BoxLayout(bookTitlePanel, BoxLayout.Y_AXIS));
         JLabel bookLabel = new JLabel("Book Title");
         JTextField bookTitleField = new JTextField("");
-        bookTitleField.setPreferredSize(new Dimension(200, 30));
-        bookTitleField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
         bookTitlePanel.add(bookLabel);
         bookTitlePanel.add(bookTitleField);
 
@@ -43,8 +69,6 @@ public class DashboardView extends JFrame {
         bookIdPanel.setLayout(new BoxLayout(bookIdPanel, BoxLayout.Y_AXIS));
         JLabel bookIdLabel = new JLabel("Book ID");
         JTextField bookIdField = new JTextField("");
-        bookIdField.setPreferredSize(new Dimension(200, 30));
-        bookIdField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
         bookIdPanel.add(bookIdLabel);
         bookIdPanel.add(bookIdField);
 
@@ -52,8 +76,6 @@ public class DashboardView extends JFrame {
         authorDataPanel.setLayout(new BoxLayout(authorDataPanel, BoxLayout.Y_AXIS));
         JLabel authorLabel = new JLabel("Author Name");
         JTextField authorField = new JTextField("");
-        authorField.setPreferredSize(new Dimension(200, 30));
-        authorField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
         authorDataPanel.add(authorLabel);
         authorDataPanel.add(authorField);
 
@@ -61,8 +83,6 @@ public class DashboardView extends JFrame {
         publisherDataPanel.setLayout(new BoxLayout(publisherDataPanel, BoxLayout.Y_AXIS));
         JLabel publisherLabel = new JLabel("Publisher");
         JTextField publisherField = new JTextField("");
-        publisherField.setPreferredSize(new Dimension(200, 30));
-        publisherField.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
         publisherDataPanel.add(publisherLabel);
         publisherDataPanel.add(publisherField);
 
@@ -71,10 +91,14 @@ public class DashboardView extends JFrame {
         JLabel publicationLabel = new JLabel("Publication Date");
         DateFormat df = new SimpleDateFormat("MM/dd/yyyy");
         JFormattedTextField txtDate = new JFormattedTextField(df);
-        txtDate.setPreferredSize(new Dimension(200, 30));
-        txtDate.setMaximumSize(new Dimension(Integer.MAX_VALUE, 30));
         publicationDataPanel.add(publicationLabel);
         publicationDataPanel.add(txtDate);
+
+        JPanel searchPanel = new JPanel();
+        searchPanel.setLayout(new BoxLayout(searchPanel, BoxLayout.Y_AXIS));
+        JTextField searchField = new JTextField("");
+        searchPanel.add(searchField);
+        searchPanel.add(new Button("Search book"));
 
         JButton addBook = new JButton("Add new book");
         addBook.addActionListener(e -> {
@@ -94,7 +118,40 @@ public class DashboardView extends JFrame {
         parentPanel.add(publisherDataPanel);
         parentPanel.add(publicationDataPanel);
         parentPanel.add(buttonPanel);
+        parentPanel.add(searchPanel);
 
         return  parentPanel;
+    }
+
+
+    private static JPanel createUserLeftPanel() {
+        JPanel parentPanel = new JPanel();
+        parentPanel.setLayout(new GridLayout(2, 1));
+
+        JButton showBooks = new JButton("Show Books");
+        JButton returnBooks = new JButton("Return Books");
+        JButton logOut = new JButton("Log Out");
+
+        parentPanel.add(showBooks);
+        parentPanel.add(returnBooks);
+        parentPanel.add(logOut);
+        return  parentPanel;
+    }
+
+    public void setColumnNames() {
+        booksTable.addColumn("Book Title");
+        booksTable.addColumn("Book ID");
+        booksTable.addColumn("Author");
+        booksTable.addColumn("Status");
+
+        // if manager
+//        booksTable.addColumn("ID of holder");
+    }
+
+    public void setData() {
+        booksTable.setRowCount(0);
+        for (int i = 0; i < 20; i++) {
+            booksTable.addRow(new String[]{"C:+" + i});
+        }
     }
 }
